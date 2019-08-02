f7ccdd84a50370c7f2fbde7b689db87236211705
Brian Foley <bfoley-teamug@users.noreply.github.com>
2019-08-02 09:22:49 -0700

    GET and POST form

---
+++ contact.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Get and post</title>
+<link href="styles.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<h1>Contact Us</h1>
+<form method="get" action="<?= $_SERVER['PHP_SELF']; ?>">
+  <p>
+    <label for="name">Name:</label>
+    <input type="text" name="name" id="name">
+  </p>
+  <p>
+    <label for="email">Email:</label>
+    <input type="email" name="email" id="email">
+  </p>
+  <p>
+    <label for="comments">Comments:</label>
+    <textarea name="comments" id="comments"></textarea>
+  </p>
+  <p>
+    <input type="submit" name="send" id="send" value="Send Comments">
+  </p>
+</form>
+<pre>
+  <?php
+    if($_GET) {
+      echo 'Content of the $_GET array:<br>';
+      print_r($_GET);
+    } elseif($_POST) {
+      echo 'Content of the $_POST array:<br>';
+      pring_r($_POST);
+    }
+
+   ?>
+
+</pre>
+</body>
+</html>
